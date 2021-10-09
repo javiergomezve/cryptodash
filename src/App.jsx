5d3b2c3e3d6a90853071be5567c49a612d0866cf
Javier Gomez <javiergomezve@gmail.com>
2021-10-09 12:26:42 -0500

    [ADD] Base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import './App.css';
+import {
+    Navbar,
+    Home,
+    Exchanges,
+    Cryptocurrencies,
+    CryptoDetails,
+    News,
+} from './components';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Switch>
+                            <Route exact path="/">
+                                <Home />
+                            </Route>
+                            <Route exact path="/exchanges">
+                                <Exchanges />
+                            </Route>
+                            <Route exact path="/cryptocurrencies">
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path="/cryptocurrencies/:coinId">
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path="/news">
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
+
+                <div className="footer">
+                    <Typography.Title
+                        level={5}
+                        style={{ color: 'white', textAlign: 'center' }}
+                    >
+                        Cryptodash <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
